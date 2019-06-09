7c50aa561d12a17c0ce168242821fd19d6b05004
Andrew Hemminger <arhemminger@gmail.com>
2019-06-08 23:08:33 -0500

    adding code comments

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,14 +1,14 @@
 /*
 ============================================
-; Title:  API Gateway Part II
+; Title:  API Gateway Part IV
 ; Author: Professor Krasso
-; Date:  12 May 2019
+; Date:  8 June 2019
 ; Modified by: Andrew Hemminger
-; Description: Exercise 2.3 - API Gateway Part II
+; Description: Exercise 6.3 - API Gateway Part IV
 ;===========================================
 */
 var header = require('../hemminger-header');
-console.log(header.display('Andrew', 'Hemminger', 'Exercise 2.3'));
+console.log(header.display('Andrew', 'Hemminger', 'Exercise 6.3'));
 console.log('\n');
 
 // start program
